9d91b9ae63ed893719c9b4f6e86216175edd77f3
ogzCode <oguzguc44@gmail.com>
2025-11-25 15:28:33 +0300

    feat: implement HomeScreen with navigation and theme toggle

---
+++ src/screen/Home.tsx
@@ -0,0 +1,56 @@
+import { View, Text, Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import type { NavigationProp } from "@react-navigation/native";
+import { ArrowRight } from "lucide-react-native";
+import { useTheme } from "@/theme/ThemeProvider";
+
+type RootStackParamList = {
+	Badges: undefined;
+	Avatars: undefined;
+	Accordions: undefined;
+	Alerts: undefined;
+};
+
+export const HomeScreen = () => {
+	const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+	const { setTheme, theme } = useTheme();
+	return (
+		<View className="flex-1 gap-4 p-4 bg-white dark:bg-gray-900">
+			<Pressable
+				className="bg-gray-900 dark:bg-gray-700 px-6 py-3 rounded-lg flex justify-between items-center flex-row"
+				onPress={() => setTheme(theme === "light" ? "dark" : "light")}
+			>
+				<Text className="text-white font-semibold">Toggle Theme</Text>
+			</Pressable>
+
+			<Pressable
+				className="bg-blue-500 dark:bg-blue-700 px-6 py-3 rounded-lg flex justify-between items-center flex-row"
+				onPress={() => navigation.navigate("Badges")}
+			>
+				<Text className="text-white font-semibold">Badge Screen</Text>
+				<ArrowRight className="absolute left-4 top-3" size={20} color="white" />
+			</Pressable>
+			<Pressable
+				className="bg-blue-500 dark:bg-blue-700 px-6 py-3 rounded-lg flex justify-between items-center flex-row"
+				onPress={() => navigation.navigate("Avatars")}
+			>
+				<Text className="text-white font-semibold">Avatar Screen</Text>
+				<ArrowRight className="absolute left-4 top-3" size={20} color="white" />
+			</Pressable>
+			<Pressable
+				className="bg-blue-500 dark:bg-blue-700 px-6 py-3 rounded-lg flex justify-between items-center flex-row"
+				onPress={() => navigation.navigate("Accordions")}
+			>
+				<Text className="text-white font-semibold">Accordion Screen</Text>
+				<ArrowRight className="absolute left-4 top-3" size={20} color="white" />
+			</Pressable>
+			<Pressable
+				className="bg-blue-500 dark:bg-blue-700 px-6 py-3 rounded-lg flex justify-between items-center flex-row"
+				onPress={() => navigation.navigate("Alerts")}
+			>
+				<Text className="text-white font-semibold">Alert Dialog Screen</Text>
+				<ArrowRight className="absolute left-4 top-3" size={20} color="white" />
+			</Pressable>
+		</View>
+	);
+}
