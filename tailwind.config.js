0cf08709574709417e1e1d057bb556d0507db154
ogzCode <oguzguc44@gmail.com>
2025-11-25 15:28:08 +0300

    fix: update content paths in tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}', './src/**/*.{js,ts,tsx}'],
 
   presets: [require('nativewind/preset')],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
