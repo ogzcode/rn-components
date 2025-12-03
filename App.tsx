d28c743d7dbc6efee26e4568f2c92d56ba126a8a
ogzCode <oguzguc44@gmail.com>
2025-12-03 14:18:34 +0300

    feat: wrap App component in SafeAreaProvider for improved layout handling

--- App.tsx
+++ App.tsx
@@ -3,13 +3,19 @@ import { NavContainer } from 'src/navigation/NavContainer';
 import ThemeProvider from '@/theme/ThemeProvider';
 import './global.css';
 import { PortalHost } from '@rn-primitives/portal';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ToastProvider } from '@/ui/toast/useToast';
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <NavContainer />
-      <PortalHost />
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaProvider style={{ flex: 1 }}>
+      <ThemeProvider>
+        <ToastProvider>
+          <NavContainer />
+        </ToastProvider>
+        <PortalHost />
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
